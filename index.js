24a9fb22fde32341eed134d6ca3c6980e030775b
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2021-01-04 23:01:40 +0000

    playing around with axios

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-console.log('Hi there!');
+// console.log('Hi there!');
 
 //network hhtp request using third party library Axios
-//step 1 set up a helper function to fetch the data
+//step 1 define and set up a helper function to fetch the data
+//will want to use async/await syntax when making the request
+
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '8b6de0c4',
+      s: 'avengers'
+      // t: 'the',
+      // type: 'movie'
+    }
+  })
+  const movies = response.data
+  console.log(response.data)
+  for (let movie in movies.Search[0]) {
+    console.log(movie)
+  }
+}
+
+fetchData()
